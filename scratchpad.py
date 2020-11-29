e35107cce86cbf47e09c26b9cd071de375522b7e
stevenmerino <10862416+stevenmerino@users.noreply.github.com>
2020-11-28 23:43:43 -0700

    get data

--- scratchpad.py
+++ scratchpad.py
@@ -8,31 +8,25 @@ import pytz
 from datetime import datetime
 from datetime import timezone
 
+from matplotlib import pyplot
 
 
-#start_date = "1605537000"
-#end_date = "1605537060"
-#ny_tz = pytz.timezone('US/Eastern')
-#start_date = ny_tz.localize(datetime(2020,11,2,9,30))
-#end_date = ny_tz.localize(datetime(2020,11,3))
-#start_date_ts = str(int(start_date.replace().timestamp()))
-#end_date_ts = str(int(end_date.replace().timestamp()))
-
-#spy = get_candle("SPY", start_date, end_date, "D")
-start_date = dt_str(datetime(2020,11,25,9,30))
-end_date = dt_str(datetime(2020,11,25,9,31))
+start_date = dt_str(datetime(2020,11,25))
+end_date = dt_str(datetime(2020,11,26))
 
 print(start_date)
 print(end_date)
+print("="*80)
 
-print(get_candle("SPY", start_date, end_date))
+# save this to a file and load it rather than pull it everytime
+data = get_candle("SPY", start_date, end_date)
 
+print(data)
+print("="*80)
 
+data1 = data["c"]
+data2 = data["v"]
 
-# symbol = "SPY"
-
-# spy = requests.get('https://finnhub.io/api/v1/stock/candle?symbol=' + symbol + '&resolution=1&from=' + start_date + '&to=' + end_date + '&token=' + finnhub_api_key)
-
-#print(spy.json().values())
-
+pyplot.scatter(data1, data2)
+pyplot.show()
 
